f81464f19a44192319c4d6a49e159f8a7edb81cf
GoldFireGames <jakub.glonek@o2.pl>
2023-02-21 13:15:53 +0100

    startup project

---
+++ DeviceSdkDemo (Ex1)/DeviceSdkDemo.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using DeviceSdkDemo.Device;
+using Microsoft.Azure.Devices.Client;
+
+string deviceConnectionString = "HostName=name-test-ul.azure-devices.net;DeviceId=jm-221013-2;SharedAccessKey=+BKFUpG43eKWe0d9oSXrdGGEwJlAuiq7ugfFP+f0dpE=";
+
+using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+await deviceClient.OpenAsync();
+var device = new VirtualDevice(deviceClient);
+
+await device.InitializeHandlers();
+await device.UpdateTwinAsync();
+
+Console.WriteLine($"Connection success!");
+
+await device.SendMessages(3, 1000);
+
+Console.WriteLine("Finished! Press Enter to close...");
+Console.ReadLine();
